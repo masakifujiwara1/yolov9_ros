166067706866f770726416bcd46b981c16090ab3
MasakiFujiwara <fmasaki2020@gmail.com>
2024-10-14 02:49:41 +0900

    Add files

---
+++ yolov9_ros/yolov8_ros/bev2graph.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+import rospy
+import math
+from collections import defaultdict, deque
+from sensor_msgs.msg import LaserScan
+from yolov8_msgs.msg import DetectionArray
+from ptp_msgs.msg import PedestrianArray
+from geometry_msgs.msg import PoseArray
+from visualization_msgs.msg import Marker, MarkerArray
+import numpy as np
+
+class Bev2GraphNode:
+    def __init__(self):
+        rospy.init_node('bev2graph_node', anonymous=True)
+        self.scan_sub = rospy.Subscriber(
+            '/scan',
+            LaserScan,
+            self.callback)
+        self.detections_sub = rospy.Subscriber(
+            '/yolo/tracking',
+            DetectionArray,
+            self.callback_yolo)
+        self.marker_array_pub = rospy.Publisher('detect_human', MarkerArray, queue_size=10)
+        self.pedestrian_array_pub = rospy.Publisher('ped_seq', PedestrianArray, queue_size=10)
+        self.dicts = defaultdict(lambda: {'id': 0, 'score': 0, 'theta': 0, 'x': 0, 'y': 0, 'size_x': 0, 'size_y': 0})
+        self.scan = LaserScan()
+        self.marker_array = MarkerArray()
+
+        # process frame
+        self.frame = 0
+        self.data_array = None
+        self.is_fst_flag = True
+        self.curr_frames = deque(maxlen=8)
+        rospy.Timer(rospy.Duration(0.4), self.process_frames)
+
+    def callback_yolo(self, data):
+        self.dicts.clear()
+        self.marker_array = MarkerArray()
+        for idx, i in enumerate(data.detections):
+            if i.class_id == 0:
+                self.dicts[idx]['id'] = i.id
+                self.dicts[idx]['score'] = i.score
+                self.dicts[idx]['theta'] = i.bbox.center.theta
+                self.dicts[idx]['size_x'] = i.bbox.size.x
+                self.dicts[idx]['size_y'] = i.bbox.size.y
+
+    def calc_pose(self):
+        for key, value in self.dicts.items():
+            angle = value['theta']
+            id_ = value['id']
+            score = value['score']
+            size_x = value['size_x']
+            size_y = value['size_y']
+
+            if angle < self.scan.angle_min or angle > self.scan.angle_max:
+                rospy.logwarn(f'指定した角度が範囲外です. angle={angle}, min={self.scan.angle_min}')
+                return
+
+            index = int((angle - self.scan.angle_min) / self.scan.angle_increment)
+
+            if 0 <= index < len(self.scan.ranges):
+                min_dist = float('inf')
+                for offset in range(-7, 7):
+                    distance = self.scan.ranges[index + offset]
+                    min_dist = min(distance, min_dist)
+                
+                distance = min_dist
+                x, y = self.calc_xy(angle, distance)
+
+                self.dicts[key]['x'] = x
+                self.dicts[key]['y'] = y
+
+                if distance != float('inf'):
+                    self.add_marker(id_, x, y)
+            else:
+                rospy.logwarn('計算されたインデックスが範囲外です')
+
+        self.publish_marker_array()
+
+    def callback(self, scan):
+        self.scan = scan
+
+    def calc_xy(self, angle, distance):
+        x = distance * math.cos(angle)
+        y = distance * math.sin(angle)
+        return x, y
+
+    def is_valid_index(self, i, length):
+        return 0 <= i < length
+
+    def add_marker(self, id_, x, y):
+        point = Marker()
+        point.header.frame_id = 'base_scan'
+        point.ns = "point"
+        point.id = int(id_)
+        point.type = Marker.SPHERE
+        point.action = Marker.ADD
+        point.pose.position.x = x
+        point.pose.position.y = y
+        point.pose.position.z = 0.0
+        point.scale.x = point.scale.y = point.scale.z = 1.0
+        point.color.r = 0.0
+        point.color.g = 1.0
+        point.color.b = 0.0
+        point.color.a = 1.0
+        point.lifetime = rospy.Duration(0.1)
+        self.marker_array.markers.append(point)
+    
+    def publish_marker_array(self):
+        self.marker_array_pub.publish(self.marker_array)
+
+    def process_frames(self, event):
+        self.curr_frames.append(self.frame)
+        self.calc_pose()
+        for key, value in self.dicts.items():
+            data = np.array([self.frame, self.dicts[key]['id'], self.dicts[key]['x'], self.dicts[key]['y']], dtype=np.float32)
+
+            if self.is_fst_flag:
+                self.data_array = data
+                self.is_fst_flag = False
+            else:
+                self.data_array = np.vstack((self.data_array, data))
+
+        if len(self.curr_frames) == 8:
+            self.data_array = self.data_array[self.data_array[:, 0].astype(int) >= self.curr_frames[0]]
+
+            msg = PedestrianArray()
+            msg.data = self.data_array.flatten().tolist()
+            msg.shape = self.data_array.shape
+            msg.dtype = str(self.data_array.dtype)
+
+            self.pedestrian_array_pub.publish(msg)
+
+        self.frame += 24
+
+def main():
+    bev2graph_node = Bev2GraphNode()
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
---
+++ yolov9_ros/yolov8_ros/tracking_node.py
@@ -0,0 +1,132 @@
+import numpy as np
+import math
+
+import rospy
+from cv_bridge import CvBridge
+
+from ultralytics.trackers import BOTSORT, BYTETracker
+from ultralytics.trackers.basetrack import BaseTrack
+from ultralytics.utils import IterableSimpleNamespace, yaml_load
+from ultralytics.utils.checks import check_requirements, check_yaml
+from ultralytics.engine.results import Boxes
+
+from sensor_msgs.msg import Image, CompressedImage
+from yolov9_msgs.msg import Detection
+from yolov9_msgs.msg import DetectionArray
+from std_srvs.srv import SetBool
+import message_filters
+
+
+class TrackingNode:
+
+    def __init__(self) -> None:
+        rospy.init_node("tracking_node")
+
+        self.cv_bridge = CvBridge()
+
+        tracker_name = rospy.get_param("~tracker", "bytetrack.yaml")
+
+        self.tracker = self.create_tracker(tracker_name)
+        self._pub = rospy.Publisher("tracking", DetectionArray, queue_size=10)
+
+        # subs
+        self.image_sub = message_filters.Subscriber(
+            "/image_raw", CompressedImage)
+        self.detections_sub = message_filters.Subscriber(
+            "/detections", DetectionArray)
+
+        self._synchronizer = message_filters.ApproximateTimeSynchronizer(
+            [self.image_sub, self.detections_sub], 10, 0.5)
+        self._synchronizer.registerCallback(self.detections_cb)
+
+    def create_tracker(self, tracker_yaml: str) -> BaseTrack:
+
+        TRACKER_MAP = {"bytetrack": BYTETracker, "botsort": BOTSORT}
+        check_requirements("lap")  # for linear_assignment
+
+        tracker = check_yaml(tracker_yaml)
+        cfg = IterableSimpleNamespace(**yaml_load(tracker))
+
+        assert cfg.tracker_type in ["bytetrack", "botsort"], \
+            f"Only support 'bytetrack' and 'botsort' for now, but got '{cfg.tracker_type}'"
+        tracker = TRACKER_MAP[cfg.tracker_type](args=cfg, frame_rate=1)
+        return tracker
+
+    def calc_theta(self, w_img_, x_center_):
+        fov_horizonal = math.pi / 2  # 90deg
+        theta_ = -((x_center_ - (w_img_ / 2)) * fov_horizonal) / w_img_
+        theta_ = math.atan2(math.sin(theta_), math.cos(theta_))
+        return theta_
+
+    def detections_cb(self, img_msg: CompressedImage, detections_msg: DetectionArray) -> None:
+
+        tracked_detections_msg = DetectionArray()
+        tracked_detections_msg.header = img_msg.header
+
+        # convert image
+        cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(img_msg)
+
+        # parse detections
+        detection_list = []
+        detection: Detection
+        for detection in detections_msg.detections:
+
+            detection_list.append(
+                [
+                    detection.bbox.center.position.x - detection.bbox.size.x / 2,
+                    detection.bbox.center.position.y - detection.bbox.size.y / 2,
+                    detection.bbox.center.position.x + detection.bbox.size.x / 2,
+                    detection.bbox.center.position.y + detection.bbox.size.y / 2,
+                    detection.score,
+                    detection.class_id
+                ]
+            )
+
+        img_msg = self.cv_bridge.cv2_to_imgmsg(cv_image, encoding="bgr8")
+
+        # tracking
+        if len(detection_list) > 0:
+
+            det = Boxes(
+                np.array(detection_list),
+                (img_msg.height, img_msg.width)  # 720, 1280 fov H: 90 V: 59
+            )
+
+            tracks = self.tracker.update(det, cv_image)
+
+            if len(tracks) > 0:
+
+                for t in tracks:
+
+                    tracked_box = Boxes(
+                        t[:-1], (img_msg.height, img_msg.width))
+
+                    tracked_detection: Detection = detections_msg.detections[int(
+                        t[-1])]
+
+                    # get boxes values
+                    box = tracked_box.xywh[0]
+                    tracked_detection.bbox.center.position.x = float(box[0])
+                    tracked_detection.bbox.center.position.y = float(box[1])
+
+                    tracked_detection.bbox.center.theta = self.calc_theta(img_msg.width, float(box[0]))
+
+                    tracked_detection.bbox.size.x = float(box[2])
+                    tracked_detection.bbox.size.y = float(box[3])
+
+                    # get track id
+                    track_id = ""
+                    if tracked_box.is_track:
+                        track_id = str(int(tracked_box.id))
+                    tracked_detection.id = track_id
+
+                    # append msg
+                    tracked_detections_msg.detections.append(tracked_detection)
+
+        # publish detections
+        self._pub.publish(tracked_detections_msg)
+
+
+if __name__ == '__main__':
+    node = TrackingNode()
+    rospy.spin()
