9d84197e5cd515d965b5eba61a46a97a3f6d01cf
Masaki Fujiwara <72371743+masakifujiwara1@users.noreply.github.com>
2024-11-02 15:45:02 +0900

    Dataset devel (#1)
    * Update search range
    
    * Support publishing data for individual frame
    
    * Update: Remove data with miscalculated positions
    
    * Modify  log output content

--- yolov9_ros/scripts/bev2graph.py
+++ yolov9_ros/scripts/bev2graph.py
@@ -24,6 +24,7 @@ class Bev2GraphNode:
             self.callback_yolo)
         self.marker_array_pub = rospy.Publisher('detect_human', MarkerArray, queue_size=10)
         self.pedestrian_array_pub = rospy.Publisher('ped_seq', PedestrianArray, queue_size=10)
+        self.curr_ped_array_pub = rospy.Publisher('curr_ped', PedestrianArray, queue_size=10)
         self.dicts = defaultdict(lambda: {'id': 0, 'score': 0, 'theta': 0, 'x': 0, 'y': 0, 'size_x': 0, 'size_y': 0, 'distance': 0})
         self.scan = LaserScan()
         self.marker_array = MarkerArray()
@@ -114,7 +115,14 @@ class Bev2GraphNode:
                 distance = min_dist
                 x, y = self.calc_xy(angle, distance)
 
-                x, y, _ = transform_pose(x, y, 0.0)
+                # print(f'base_link_x: {x}, base_link_y: {y}')
+
+                if x == 0.0 or y == 0.0:
+                    x = float('inf')
+                    y = float('inf')
+                    rospy.logwarn(f'Detect position calculation errors')
+                else:
+                    x, y, _ = transform_pose(x, y, 0.0)
 
                 self.dicts[key]['x'] = x
                 self.dicts[key]['y'] = y
@@ -227,7 +235,11 @@ class Bev2GraphNode:
 
                 if abs(self.dicts[key]['theta']) >= 2.30:
                     continue
-                
+
+                if self.dicts[key]['x'] == float('inf') or self.dicts[key]['y'] == float('inf'):
+                    rospy.loginfo(f'Exclude data with calc err!')
+                    continue
+
                 data = np.array([self.frame, self.dicts[key]['id'], self.dicts[key]['x'], self.dicts[key]['y']], dtype=np.float32)
 
                 if self.is_fst_flag:
@@ -235,6 +247,16 @@ class Bev2GraphNode:
                     self.is_fst_flag = False
                 else:
                     self.data_array = np.vstack((self.data_array, data))
+
+            curr_data_array = self.data_array[self.data_array[:, 0].astype(int) == self.frame]
+
+            curr_msg = PedestrianArray()
+            curr_msg.data = curr_data_array.flatten().tolist()
+            curr_msg.shape = curr_data_array.shape
+            curr_msg.dtype = str(curr_data_array.dtype)
+
+            self.curr_ped_array_pub.publish(curr_msg)
+                
         except:
             rospy.loginfo("error")
 
